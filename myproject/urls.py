e4ec8979dcdff8ca609fa59f05f939899b879fdb
CarlosAlfredodelaCruzAlvarado <128069859+CarlosAlfredodelaCruzAlvarado@users.noreply.github.com>
2024-09-17 06:50:53 -0600

    Update urls.py

--- myproject/urls.py
+++ myproject/urls.py
@@ -2,7 +2,8 @@
 
 from django.contrib import admin
 from django.urls import path, include
-from .views import ComplaintList
+from complaints.views import ComplaintList
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
