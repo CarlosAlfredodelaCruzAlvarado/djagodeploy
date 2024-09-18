5b1c486a4284379fd97053dc3026267dccc1f86d
CarlosAlfredodelaCruzAlvarado <128069859+CarlosAlfredodelaCruzAlvarado@users.noreply.github.com>
2024-09-17 23:37:57 -0600

    Update urls.py

--- myproject/urls.py
+++ myproject/urls.py
@@ -9,6 +9,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('complaints.urls')),  # API routes
     path('', include('complaints.urls')),  # Frontend routes if applicable
-        path('complaints/', ComplaintList.as_view(), name='complaints'),
+        #path('complaints/', ComplaintList.as_view(), name='complaints'),
 
 ]
